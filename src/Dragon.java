eb7d08d3b3ed84dab6461c42d3e7b89a6a1631c7
Tartaglia11 <wilax175@gmail.com>
2023-03-31 19:15:32 +0600

    deleted chance of fireball in 'releaseFireBall' method

--- src/Dragon.java
+++ src/Dragon.java
@@ -44,16 +44,11 @@ public class Dragon {
 
     public void attack(Knight knight) {
         knight.setHitPoints(knight.getHitPoints() - (damage - knight.getDefence()));
-        System.out.println("The dragon attacked you! (-" + (damage - knight.getDefence()) +")");
+        System.out.println("The dragon attacked you! (-" + (damage - knight.getDefence()) + ")");
     }
 
     public void releaseFireBall(Knight knight) {
-        int chanceOfFireBall = new Random().nextInt(1, 5); //chance is 25%
-        if (chanceOfFireBall == 1) {
-            knight.setHitPoints(knight.getHitPoints() - 350); //Fireball damage
-            System.out.println("The dragon sent you a fire ball! Your hp: " + knight.getHitPoints());
-        } else {
-            knight.setHitPoints(knight.getHitPoints() - damage);
-        }
+        knight.setHitPoints(knight.getHitPoints() - 350); //Fireball damage
+        System.out.println("The dragon sent you a fire ball! Your hp: " + knight.getHitPoints());
     }
 }
