a732b8ae6ca5ddf9acea7c96952f9a21f660195e
Tartaglia11 <wilax175@gmail.com>
2023-03-31 18:37:30 +0600

    added 'getNeedNumber' method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Dragon dragon = new Dragon();
+        Knight knight = new Knight();
     }
+
+    public static int getNeedNumber(int minValue, int maxValue) {
+        Scanner sc = new Scanner(System.in);
+        int number;
+
+        do {
+            number = sc.nextInt();
+        } while (number < minValue || number >= maxValue);
+
+        return number;
+    }
+
 }
