9a4ca240fd41655291123b7a9a35ad4fae861ffb
Tartaglia11 <wilax175@gmail.com>
2023-03-31 20:21:00 +0600

    fixed infinite game loop bug

--- src/Main.java
+++ src/Main.java
@@ -3,57 +3,58 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        initializeGame();
+    }
+
+    public static void initializeGame() {
         Dragon dragon = new Dragon();
         Knight knight = new Knight();
 
-        while (dragon.getHitPoints() > 0 || knight.getHitPoints() > 0) {
-            initializeGame(dragon, knight);
-        }
-    }
+        while (dragon.getHitPoints() > 0 && knight.getHitPoints() > 0){
+            Random random = new Random();
 
-    public static void initializeGame(Dragon dragon, Knight knight) {
-        Random random = new Random();
+            System.out.printf("Your hp: %d%n", knight.getHitPoints());
+            System.out.printf("Dragon's hp: %d%n", dragon.getHitPoints());
 
-        System.out.printf("Your hp: %d%n", knight.getHitPoints());
-        System.out.printf("Dragon's hp: %d%n", dragon.getHitPoints());
+            System.out.println();
 
-        System.out.println();
-
-        System.out.print("""
+            System.out.print("""
                 Enter to select an action:
                 1 - attack
                 2 - try to block
                 3 - heal (+220)
                 \t:""");
 
-        int action = getNeedNumber(1, 4);
+            int action = getNeedNumber(1, 4);
 
-        switch (action) {
-            case 1 -> knight.attack(dragon);
-            case 2 -> knight.tryToBlock(dragon);
-            case 3 -> knight.heal();
-            default -> System.out.println("Critical error! Something went wrong!");
-        }
+            switch (action) {
+                case 1 -> knight.attack(dragon);
+                case 2 -> knight.tryToBlock(dragon);
+                case 3 -> knight.heal();
+                default -> System.out.println("Critical error! Something went wrong!");
+            }
 
-        try {
-            Thread.sleep(1400);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } // A little pause to don't get confused
+            try {
+                Thread.sleep(1400);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } // A little pause to don't get confused
 
-        int dragonAttackType = random.nextInt(1, 5);
-        if (dragonAttackType == 1) {
-            dragon.releaseFireBall(knight);
-        } else {
-            dragon.attack(knight);
-        }
+            int dragonAttackType = random.nextInt(1, 5);
+            if (dragonAttackType == 1) {
+                dragon.releaseFireBall(knight);
+            } else {
+                dragon.attack(knight);
+            }
 
-        System.out.println("""
+            System.out.println("""
                 
                 
                 -------------------------------------------------------------------
                 
                 """);
+        }
+
     }
 
     public static int getNeedNumber(int minValue, int maxValue) {
