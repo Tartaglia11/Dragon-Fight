3585aad7303d97a168599ab5a1d1a648f58e6e55
Tartaglia11 <wilax175@gmail.com>
2023-03-31 18:37:56 +0600

    added method to start the game

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,23 @@ public class Main {
         Knight knight = new Knight();
     }
 
+    public static void initializeGame(Dragon dragon, Knight knight) {
+        System.out.print("""
+                Enter to select an action:
+                1 - attack
+                2 - try to block
+                3 - heal (+220)""");
+
+        int action = getNeedNumber(1, 4);
+
+        switch (action) {
+            case 1 -> knight.attack(dragon);
+            case 2 -> knight.tryToBlock(dragon);
+            case 3 -> knight.heal();
+            default -> System.out.println("Critical error! Something went wrong!");
+        }
+    }
+
     public static int getNeedNumber(int minValue, int maxValue) {
         Scanner sc = new Scanner(System.in);
         int number;
