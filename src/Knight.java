5af0cba3828738ba618c3b3d1951911981c1f34f
Tartaglia11 <wilax175@gmail.com>
2023-03-31 18:24:48 +0600

    written test logic of 'Knight' class

--- src/Knight.java
+++ src/Knight.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Knight {
 
     private int hitPoints;
@@ -16,6 +18,24 @@ public class Knight {
         this.defence = defence;
     }
 
+    public void attack(Dragon dragon) {
+        dragon.setHitPoints(dragon.getHitPoints() - damage);
+        System.out.printf("%d damage! Now the dragon have a %d hp%n", damage, dragon.getHitPoints());
+    }
 
+    public void tryToBlock(Dragon dragon) {
+        Random random = new Random();
+        int chanceToBlock = random.nextInt(1, 11);
+        if (chanceToBlock > 7) {
+            System.out.println("The dragon crushed your block!");
+        } else {
+            hitPoints += dragon.getDamage();
+        }
+    }
+
+    public void heal() {
+        hitPoints += 220;
+        System.out.println("You drank magic water. Your hp: " + hitPoints);
+    }
 
 }
