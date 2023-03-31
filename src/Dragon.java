6026ac516a4a5e81ad970ec85354963ed837c466
Tartaglia11 <wilax175@gmail.com>
2023-03-31 18:38:17 +0600

    added test logic of 'Dragon' class

--- src/Dragon.java
+++ src/Dragon.java
@@ -1,7 +1,10 @@
+import java.util.Random;
+
 public class Dragon {
     private int hitPoints;
     private int damage;
     private int defence;
+
 
     public Dragon() {
         hitPoints = 2500;
@@ -13,5 +16,44 @@ public class Dragon {
         this.hitPoints = hitPoints;
         this.damage = damage;
         this.defence = defence;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
+    public void attack(Knight knight) {
+        knight.setHitPoints(knight.getHitPoints() - damage);
+        System.out.println("The dragon attacked you! (-" + damage +")");
+    }
+
+    public void releaseFireBall(Knight knight) {
+        int chanceOfFireBall = new Random().nextInt(1, 5); //chance is 25%
+        if (chanceOfFireBall == 1) {
+            knight.setHitPoints(knight.getHitPoints() - 350); //Fireball damage
+            System.out.println("The dragon sent you a fire ball! Your hp: " + knight.getHitPoints());
+        } else {
+            knight.setHitPoints(knight.getHitPoints() - damage);
+        }
     }
 }
